24a2da6cd3ee80f18d695ef7adbae2c27e6e161e
Alp Bozaci <alp.bozaci@solnet.ch>
2018-11-14 21:52:49 +0100

    implementing

--- Homepage/src/main/java/ch/bozaci/homepage/presentation/RabbitServlet.java
+++ Homepage/src/main/java/ch/bozaci/homepage/presentation/RabbitServlet.java
@@ -3,6 +3,7 @@ package ch.bozaci.homepage.presentation;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class RabbitServlet
  */
+@WebServlet("/rabbitServlet")
 public class RabbitServlet extends HttpServlet
 {
     private static final long serialVersionUID = 1L;
